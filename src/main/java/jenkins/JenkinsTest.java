a5bd94a1b3fef1b627735a0863f9eff41e460f92
pro_zhao <2458047919@qq.com>
2016-05-10 10:09:56 +0800

    test

--- src/main/java/jenkins/JenkinsTest.java
+++ src/main/java/jenkins/JenkinsTest.java
@@ -4,5 +4,6 @@ public class JenkinsTest {
 	public static void main(String[] args) {
 		System.out.println("Jenkins Test");
 		System.out.println("Jenkins Test");
+		System.out.println("Jenkins Test");
 	}
 }
